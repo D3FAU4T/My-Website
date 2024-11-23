c35bfd1a7a471cb7e518985b9a569d8c56af0f84
D3FAU4T <d3fau4t@gmail.com>
2024-11-23 08:51:52 +0530

    Add router to `Assignment` Component

--- src/Components/Assignment.tsx
+++ src/Components/Assignment.tsx
@@ -1,12 +1,16 @@
+"use client";
 import { AssignmentOptions } from "@/Shared/typings";
+import { useRouter } from "next/navigation";
 
 const Assignment = (Props: AssignmentOptions) => {
+
+    const router = useRouter();
 
     const status = Props.Status === "Pending" ? "pending" :
     Props.Status === "Turned-in" ? "done_all" : "done";
 
     return (
-        <div className="note">
+        <div className="note" onClick={() => router.push('/assignments' + Props.Redirect)}>
             <div>
                 <h2>{Props.Name}</h2>
                 <div className="flag">
